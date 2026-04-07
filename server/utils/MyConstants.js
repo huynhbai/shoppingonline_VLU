8e6c9b039f53f45e6bfd90550afb021e9933e2f9
huynhbai <baop86795@gmail.com>
2026-04-07 23:37:02 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,17 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.c0jompf.mongodb.net',
+  DB_USER: 'hovietngu2004_db_user',
+  DB_PASS: 'VLU02122003',
+  DB_DATABASE: 'ShoppingOnline',
+  
+  EMAIL_USER: 'manchausahoaden7708801314520@gmail.com',
+  EMAIL_PASS: 'xgrlqchgqkaysdma',
+
+
+
+
+  JWT_SECRET: 'abciooiqwe',
+  JWT_EXPIRES: '356658435644111',
+};
+
+module.exports = MyConstants;
